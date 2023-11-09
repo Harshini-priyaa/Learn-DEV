d2b1fc5a350bfe3b1762fd59537b6b742e452111
Harshini-priyaa <harshinipriyaa1112@gmail.com>
2023-11-09 11:38:11 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Helloo</h1>
+      <h3>This is Harshini</h3>
+      <h5>Learn DEVOPS with me!</h5>
+      <P>This is where I'm going to learn to use github with Jenkins</P>
+    </div>
+  );
+}
+
+export default App;
